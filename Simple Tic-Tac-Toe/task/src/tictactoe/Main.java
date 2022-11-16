d46c045069fc7b9458ad14ee6065cdd79d271664
jakubik307 <and.jj.an@gmail.com>
2022-11-16 12:19:38 +0100

    Added simple board

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -3,5 +3,10 @@ package tictactoe;
 public class Main {
     public static void main(String[] args) {
         // write your code here
+        System.out.println("""
+                X O X
+                O X O
+                X X O
+                """);
     }
 }
