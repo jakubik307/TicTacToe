f943793281e6d51776760b2887b06e035cdd61fa
jakubik307 <and.jj.an@gmail.com>
2022-11-17 11:59:40 +0100

    Add 2 player input

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void getUserMove(char[] board) {
+    public static void getUserMove(char[] board, boolean playerXTurn) {
         Scanner scanner = new Scanner(System.in);
         boolean error;
 
@@ -25,8 +25,10 @@ public class Main {
                 } else if (board[index] == 'O' || board[index] == 'X') {
                     System.out.println("This cell is occupied! Choose another one!");
                     error = true;
-                } else {
+                } else if (playerXTurn){
                     board[index] = 'X';
+                } else {
+                    board[index] = 'O';
                 }
             } catch (InputMismatchException e) {
                 System.out.println("You should enter numbers!");
@@ -96,6 +98,7 @@ public class Main {
 
     public static void main(String[] args) {
         char[] board = new char[9];
+        boolean playerXTurn = true;
 
         for (int i = 0; i < 9; i++) {
             board[i] = ' ';
@@ -104,7 +107,8 @@ public class Main {
         printBoard(board);
 
         while (checkGameState(board)) {
-            getUserMove(board);
+            getUserMove(board, playerXTurn);
+            playerXTurn = !playerXTurn;
         }
 
 
